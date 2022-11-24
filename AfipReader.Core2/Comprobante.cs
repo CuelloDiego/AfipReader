d7d88d0ddcdd2e885562b56b66a8d6521ed136bc
Diego Cuello <cuellodiegoagustin@gmail.com>
2022-11-24 16:30:13 -0300

    Add api post

---
+++ AfipReader.Core2/Comprobante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfipReader.Core
+{
+    public class Comprobante
+    {
+        public string Nombretipocomp { get; set; } = "";
+        public double NetoNogravado { get; set; } = 0;
+        public double OpExentas { get; set; } = 0;
+        public double Total { get; set; } = 0;
+        public Importes Alicuota21 { get; set; } = new Importes();
+        public Importes Alicuota105 { get; set; } = new Importes();
+        public Importes Alicuota27 { get; set; } = new Importes();
+        public Importes AlicuotaVarias { get; set; } = new Importes();
+    }
+}
